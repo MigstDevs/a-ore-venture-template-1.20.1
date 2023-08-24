341cf3f067fd3f56316607232b6e53da4130a5dc
battl <battlebloonguy@gmail.com>
2023-08-24 12:29:43 -0300

    Thanks for PrangellPlays for helping me with the textures!

---
+++ src/main/java/net/migstmc/aoreventure/block/ModBlocks.java
@@ -0,0 +1,9 @@
+package net.migstmc.aoreventure.block;
+
+import net.migstmc.aoreventure.AOreVenture;
+
+public class ModBlocks {
+    public static void registerModBlocks() {
+        AOreVenture.LOGGER.info("Registering mod blocks for" + AOreVenture.MOD_ID);
+    }
+}
