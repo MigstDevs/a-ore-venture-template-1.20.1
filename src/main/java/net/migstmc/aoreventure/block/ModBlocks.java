1d1776409d869abd6c8e0b04a95f367e450b70ed
battl <battlebloonguy@gmail.com>
2023-08-24 13:42:52 -0300

    Thanks for PrangellPlays for helping me with the textures!

--- src/main/java/net/migstmc/aoreventure/block/ModBlocks.java
+++ src/main/java/net/migstmc/aoreventure/block/ModBlocks.java
@@ -9,10 +9,11 @@ import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
+import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 
 public class ModBlocks {
-    public static final Block RUBY_BLOCK = registerBlock("ruby_block",new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK)));
+    public static final Block RUBY_BLOCK = registerBlock("ruby_block",new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.AMETHYST_BLOCK)));
     private static Block registerBlock(String name,Block block) {
         registerBlockItem(name, block);
         return Registry.register(Registries.BLOCK, new Identifier(AOreVenture.MOD_ID, name),block);
