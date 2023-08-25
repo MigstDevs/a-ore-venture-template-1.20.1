645140ff9dfd6af90eff3aed9d1bd112c45f02a8
battl <battlebloonguy@gmail.com>
2023-08-25 16:15:19 -0300

    Thanks for PrangellPlays for helping me with the textures!

--- src/main/java/net/migstmc/aoreventure/item/custom/NewOresDetectorItem.java
+++ src/main/java/net/migstmc/aoreventure/item/custom/NewOresDetectorItem.java
@@ -1,8 +1,14 @@
 package net.migstmc.aoreventure.item.custom;
 
+import net.migstmc.aoreventure.block.ModBlocks;
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemUsageContext;
+import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
 
 public class NewOresDetectorItem extends Item {
 
@@ -12,6 +18,40 @@ public class NewOresDetectorItem extends Item {
 
     @Override
     public ActionResult useOnBlock(ItemUsageContext context) {
+        if (!context.getWorld().isClient()) {
+            BlockPos positionClicked = context.getBlockPos();
+            PlayerEntity player = context.getPlayer();
+
+            boolean foundNewlyAddedOre = false;
+
+            for (int i = 0; i <= positionClicked.getY() + 64; i++) {
+                BlockState state = context.getWorld().getBlockState(positionClicked.down(i));
+
+                if (isValuableBlock(state)) {
+                    outputValuableCoordinates(positionClicked.down(i), player, state.getBlock());
+                    foundNewlyAddedOre = true;
+
+                    break;
+                }
+            }
+
+            if (!foundNewlyAddedOre) {
+                player.sendMessage(Text.literal("Found no a ore venture ores at this position."));
+            }
+        }
+
+
+        context.getStack().damage(1, context.getPlayer(), playerEntity -> playerEntity.sendToolBreakStatus(playerEntity.getActiveHand()));
+
         return ActionResult.SUCCESS;
+    }
+
+    private void outputValuableCoordinates(BlockPos blockPos, PlayerEntity player, Block block) {
+        player.sendMessage(Text.literal("Found " + block.asItem().getName().getString() + " at " +
+                "(" + blockPos.getX() + ", " + blockPos.getY() + ", " + blockPos.getZ() + ")"), false);
+    }
+
+    private boolean isValuableBlock(BlockState state) {
+        return state.isOf(ModBlocks.RUBY_ORE);
     }
 }
